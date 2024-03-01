c90b5eb50f86d7b9e1b4774765655ff9de34aeb9
DzhEgo <maximalkin2005@gmail.com>
2024-03-01 16:58:21 +0300

    1

---
+++ test_opencart.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.get("https://ya.ru")
+
+time.sleep(5)
+driver.quit()
